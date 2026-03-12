6165899531fd88d98d2c577d8cc83f3fb7b53f0d
JuanNoackOrozco <jramirezo1200@alumno.ipn.mx>
2026-03-12 08:05:24 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-def test(a, b):
-  c = a + b
-  return c
+import sys
+
+# leer todos los datos de la entrada estándar
+datos = sys.stdin.read().split()
+
+# convertir a enteros
+a = int(datos[0])
+b = int(datos[1])
+
+# sumar
+resultado = a + b
+
+# imprimir resultado
+print(resultado)
